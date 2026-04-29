07504cd2e43646a07ff69614115f8bd4fbf4a747
BoChen SHEN <6goddddddd@gmail.com>
2026-04-29 12:49:01 +0000

    feat: add initial audio AFE implementation and header files

---
+++ components/audio/afe.c
@@ -0,0 +1,16 @@
+#include "esp_afe_sr_models.h"
+#include "esp_err.h"
+#include "esp_log.h"
+
+#define TAG "AUDIO_AFE"
+
+typedef struct
+{
+
+} audio_afe_t;
+
+esp_err_t
+audio_afe_init(audio_afe_t* afe)
+{
+    return ESP_OK;
+}
